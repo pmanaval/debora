e002646fec1c5f0d2ccb84daa3d7da9f2d2d4832
ericxiong1 <90705758+ericxiong1@users.noreply.github.com>
2022-06-17 09:48:11 -0600

    write to specific place

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ if devices[0].fullName()=="FileDevice 0":
 dev0 = devices[0]
 #device temp pg 35
 print("dev0.doSimpleAcquisition (3 frames @ 1 sec) - start")
-rc = dev0.doSimpleAcquisition(3, 1, pixet.PX_FTYPE_AUTODETECT, "example.png")
+rc = dev0.doSimpleAcquisition(3, 1, pixet.PX_FTYPE_AUTODETECT, r"FILEPATHHERE")
 print("dev0.doSimpleAcquisition - end:", rc, "(0 is OK)")
 pixet.exitPixet()
