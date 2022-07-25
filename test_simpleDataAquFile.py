1d0f38c3615caec98f56e89e3f16df3056b34153
WorkWaffle <bradon.dundas@outlook.com>
2022-07-25 09:54:58 -0600

    Updated to fix missing parameters issue, along with save loaction for files.

--- test_simpleDataAquFile.py
+++ test_simpleDataAquFile.py
@@ -12,7 +12,7 @@ if devices[0].fullName()=="FileDevice 0":
     exit()
 dev0 = devices[0]
 #device temp pg 35
-print("dev0.doAdvancedAcquisition (3 frames @ 1 sec) - start")
-rc = dev0.doAdvancedAcquisition(3, 1, pixet.PX_FTYPE_ASCII_FRAME, r"FILEPATHHERE")
+print("dev0.doAdvancedAcquisition (100 frames @ 0.1 sec) - start")
+rc = dev0.doAdvancedAcquisition(1000, 0.1,pixet.PX_ACQTYPE_FRAMES, pixet.PX_ACQMODE_NORMAL, pixet.PX_FTYPE_ASCII_FRAME,0, r"RunData\short")
 print("dev0.doAdvancedAcquisition - end:", rc, "(0 is OK)")
 pixet.exitPixet()
